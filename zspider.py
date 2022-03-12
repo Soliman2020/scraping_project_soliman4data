48559dcda2343fe37c9b53f8993d2e476ec45179
Mohamed Soliman <35764807+Soliman2020@users.noreply.github.com>
2022-03-12 17:02:20 +0200

    small updates

--- zspider.py
+++ zspider.py
@@ -2,6 +2,13 @@ import scrapy
 
 
 class ZspiderSpider(scrapy.Spider):
+    
+    '''
+    Scarping portfolio page to get the project name 
+    and its corresponding URL.
+    
+    '''
+    # spider name
     name = 'zspider'
     allowed_domains = ['https://soliman4data.wordpress.com/']
     start_urls = ['https://soliman4data.wordpress.com/portfolio']
@@ -12,19 +19,13 @@ class ZspiderSpider(scrapy.Spider):
         # projects = response.css('strong>a::text').extract()
         # links = response.css('strong>a::attr(href)').extract()
 
-        # If you use 'strong' that means directing to all strong
-        # so we got extra 6 empty rows in report.csv
-        # rows = response.css('strong')
-        
         rows = response.css('p>strong')
 
-
         for row in rows:
-
             projects = row.css('a::text').extract_first()
-            links = row.css('a::attr(href)').extract_first()
+            URL = row.css('a::attr(href)').extract_first()
 
             yield {
                 'projects_names':projects,
-                'project_links':links
+                'project_links':URL
             }
